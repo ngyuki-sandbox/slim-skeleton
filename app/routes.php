2292daf6cbb9c1045147a5a6fd28f75e7b152220
Toshiyuki Goto <ngyuki.jp@gmail.com>
2020-09-02 10:33:33 +0900

    テンプレートエンジンに Twig を使う

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,14 @@
 <?php
 declare(strict_types=1);
 
+use App\Application\Actions\HomeAction;
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Slim\Views\Twig;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -23,4 +25,6 @@ return function (App $app) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
     });
+
+    $app->get('/home', HomeAction::class);
 };

c8d931fd94fc578e3eecba6fd6867b8ca21d0b19
Toshiyuki Goto <ngyuki.jp@gmail.com>
2020-09-04 09:24:12 +0900

    slim/http のリクエスト・レスポンスを使う

--- app/routes.php
+++ app/routes.php
@@ -4,21 +4,22 @@ declare(strict_types=1);
 use App\Application\Actions\HomeAction;
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Http\Response;
+use Slim\Http\ServerRequest;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
-use Slim\Views\Twig;
 
 return function (App $app) {
-    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+    $app->options('/{routes:.*}', function (ServerRequest $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
     });
 
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello world!');
-        return $response;
+    $app->map(['GET', 'POST'], '/', function (ServerRequest $request, Response $response) {
+        if ($request->isPost()) {
+            return $response->withRedirect('http://example.com/', 303);
+        }
+        return $response->withJson(['hello' => 'world!']);
     });
 
     $app->group('/users', function (Group $group) {
